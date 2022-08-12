28e12b3f002f597d542afd7644b1b377e20c612f
Mxngls <Maximilian.e.hoenig@gmail.com>
2022-08-12 15:40:55 +0900

    Remove revalidate from getStaticProps()

--- pages/view/[id].jsx
+++ pages/view/[id].jsx
@@ -43,7 +43,7 @@ export async function getStaticPaths() {
     const paths = await getAllIds();
     return {
         paths,
-        fallback: 'blocking',
+        fallback: "blocking",
     };
 }
 
@@ -54,6 +54,5 @@ export async function getStaticProps({ params }) {
         props: {
             data,
         },
-        revalidate: 60
     };
 }
