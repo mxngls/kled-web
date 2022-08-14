e768c1deafd8af2aaea68e6d574764f859c5d80c
Mxngls <Maximilian.e.hoenig@gmail.com>
2022-08-14 16:42:31 +0900

    Change revalidate time

--- pages/view/[id].jsx
+++ pages/view/[id].jsx
@@ -53,7 +53,7 @@ export async function getStaticProps({ params }) {
     const word = await getWord(id);
 
     if (!!word) {
-        return { props: { word }, revalidate: 86400 };
+        return { props: { word }, revalidate: 5 };
     } else {
         return { notFound: true };
     }
