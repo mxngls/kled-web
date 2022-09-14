56817d743f3899c51e103530380e10b796a2122a
mxngls <Maximilian.e.hoenig@gmail.com>
2022-09-14 22:44:43 +0900

    Move state from ContentContainer into ContentBody

--- components/content/ContentBody.jsx
+++ components/content/ContentBody.jsx
@@ -1,10 +1,55 @@
 import { useRouter } from "next/router";
+import { useEffect, useCallback, useState } from "react";
 
-export default function ContentBody({ words }) {
+import WordContainer from "../word/WordContainer";
+import GoToViewButton from "../GoToViewButton";
+
+export default function ContentBody({ data, option }) {
     const router = useRouter();
+    const [state, setState] = useState({ words: data.words });
+
+    const memorizedSortedWords = useCallback(
+        (sort) => {
+            const copy = [...state.words];
+            sort === "frequency"
+                ? copy.sort((a, b) => a.item.Frequency - b.item.Frequency)
+                : sort === "name"
+                ? copy.sort((a, b) => a.item.Alpha - b.item.Alpha)
+                : null;
+            setState({ ...state, words: copy });
+        },
+        [option, router.query.sort]
+    );
+
+    useEffect(() => {
+        memorizedSortedWords(option);
+
+        return () => {};
+    }, [option, router.query.sort, memorizedSortedWords]);
+
     return (
         <div className={"content-container__body"}>
-            {words.slice(10 * router.query.batch, 10 * router.query.batch + 10)}
+            {state.words
+                .slice(10 * router.query.batch, 10 * router.query.batch + 10)
+                .map((element, index, arr) => {
+                    return (
+                        <WordContainer
+                            last={
+                                index === arr.length - 1 ||
+                                (index + 1) % 10 === 0
+                                    ? true
+                                    : false
+                            }
+                            key={element.item.Id}
+                            data={element}
+                            keyword={data.keyword}
+                        >
+                            {element.item.Inflection === null && (
+                                <GoToViewButton id={element.item.Id} />
+                            )}
+                        </WordContainer>
+                    );
+                })}
         </div>
     );
 }
--- components/content/ContentContainer.jsx
+++ components/content/ContentContainer.jsx
@@ -1,76 +1,29 @@
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/router";
 
 import ContentFooter from "./contentFooter/ContentFooterContainer";
 import ContentHeader from "./ContentHeader/ContentHeaderContainer";
 
-import WordContainer from "../word/WordContainer";
 import ContentBody from "./ContentBody";
 
-import GoToViewButton from "../GoToViewButton";
-
-export default function ContentContainer({ data, keyword, resNum }) {
+export default function ContentContainer({ data, setData }) {
     const router = useRouter();
-    const [state, setState] = useState({ words: [] });
     const [option, setOption] = useState(router.query.sort);
-
-    const memorizedSortedWords = useCallback(
-        (sort) => {
-            const copy = [].concat(data);
-
-            sort === "frequency"
-                ? copy.sort((a, b) => {
-                      return a.item.Frequency - b.item.Frequency;
-                  })
-                : sort === "name"
-                ? copy.sort((a, b) => {
-                      return a.item.Alpha - b.item.Alpha;
-                  })
-                : null;
-            setState((prevState) => ({
-                ...prevState,
-                words: copy.map((element, index, copy) => {
-                    return (
-                        <WordContainer
-                            last={
-                                index === copy.length - 1 ||
-                                (index + 1) % 10 === 0
-                                    ? true
-                                    : false
-                            }
-                            key={element.item.Id}
-                            data={element}
-                            keyword={router.query.keyword}
-                        >
-                            {element.item.Inflection === null && (
-                                <GoToViewButton id={element.item.Id} />
-                            )}
-                        </WordContainer>
-                    );
-                }),
-            }));
-        },
-        [data, router]
-    );
-
-    useEffect(() => {
-        memorizedSortedWords(option);
-    }, [option, data, router.query.keyword, memorizedSortedWords]);
 
     return (
         <div className="content-container">
             <ContentHeader
-                resNum={resNum}
-                keyword={keyword}
+                resNum={data.words.length}
+                keyword={data.keyword}
                 option={option}
                 setOption={setOption}
             />
-            <ContentBody words={state.words} />
+            <ContentBody data={data} setData={setData} option={option} />
             <ContentFooter
                 keyword={router.query.keyword}
                 matchType={router.query.matchType}
                 order={router.query.sort}
-                resNum={data.length}
+                resNum={data.words.length}
             ></ContentFooter>
         </div>
     );
