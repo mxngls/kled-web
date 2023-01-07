1c0647d88a9015793fc8a48934ca62bed518bce8
mxngls <Maximilian.e.hoenig@gmail.com>
2023-01-07 23:32:22 +0900

    Fix error when search results include inflections

--- components/word/WordBody.jsx
+++ components/word/WordBody.jsx
@@ -2,35 +2,35 @@ import SenseContainer from "./sense/SenseContainer";
 import Inflections from "./Inflections.jsx";
 
 export default function WordBody({
-    search,
-    senses,
-    inflections,
-    inflectionLinks,
-    id,
-    keyword,
+  search,
+  senses,
+  inflections,
+  inflectionLinks,
+  id,
+  keyword,
 }) {
-    const wordContainer = [];
-    if (!!senses[0].Translation) {
-        wordContainer.push(
-            <SenseContainer
-                search={search}
-                key={`word-sense-${id}`}
-                data={senses}
-                id={id}
-                keyword={keyword}
-            />
-        );
-    } else if (!!inflections) {
-        wordContainer.push(
-            <Inflections
-                key={`word-inflections-${id}`}
-                Inflections={inflections}
-                InflectionLinks={inflectionLinks}
-                id={id}
-                keyword={keyword}
-            />
-        );
-    }
+  const wordContainer = [];
+  if (!!senses) {
+    wordContainer.push(
+      <SenseContainer
+        search={search}
+        key={`word-sense-${id}`}
+        data={senses}
+        id={id}
+        keyword={keyword}
+      />
+    );
+  } else if (!!inflections) {
+    wordContainer.push(
+      <Inflections
+        key={`word-inflections-${id}`}
+        Inflections={inflections}
+        InflectionLinks={inflectionLinks}
+        id={id}
+        keyword={keyword}
+      />
+    );
+  }
 
-    return <div className="word-container__body">{wordContainer}</div>;
+  return <div className="word-container__body">{wordContainer}</div>;
 }
--- components/word/WordContainer.jsx
+++ components/word/WordContainer.jsx
@@ -1,26 +1,32 @@
 import WordHeader from "./WordHeader.jsx";
 import WordBody from "./WordBody.jsx";
 
-export default function WordContainer({ search, last, data, keyword, children }) {
-    return (
-        <div className={`word-container${last ? "--last" : ""}`}>
-            <WordHeader
-                hangul={data.item.Hangul}
-                homonymNumber={data.item.HomonymNumber}
-                hanja={data.item.Hanja}
-                typeKr={data.item.TypeKr}
-                keyword={keyword}
-            >
-                {children}
-            </WordHeader>
-            <WordBody
-                search={search}
-                senses={data.item.Senses}
-                inflections={data.item.Inflections}
-                inflectionLinks={data.item.InflectionLinks}
-                id={data.item.Id}
-                keyword={keyword}
-            />
-        </div>
-    );
+export default function WordContainer({
+  search,
+  last,
+  data,
+  keyword,
+  children,
+}) {
+  return (
+    <div className={`word-container${last ? "--last" : ""}`}>
+      <WordHeader
+        hangul={data.item.Hangul}
+        homonymNumber={data.item.HomonymNumber}
+        hanja={data.item.Hanja}
+        typeKr={data.item.TypeKr}
+        keyword={keyword}
+      >
+        {children}
+      </WordHeader>
+      <WordBody
+        search={search}
+        senses={data.item.Senses}
+        inflections={data.item.Inflection}
+        inflectionLinks={data.item.InflectionLinks}
+        id={data.item.Id}
+        keyword={keyword}
+      />
+    </div>
+  );
 }
